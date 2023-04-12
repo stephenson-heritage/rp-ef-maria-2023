ca9a173b1325c5b8c8a82f833dfc5b21e740f7c4
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2023-04-12 11:32:14 -0400

    added rating to details view

--- Data/StoreContext.cs
+++ Data/StoreContext.cs
@@ -7,10 +7,11 @@ using rp_ef_maria.Models;
 
 public class StoreContext : DbContext
 {
-    public StoreContext(DbContextOptions<StoreContext> options)
-        : base(options)
-    {
-    }
+	public StoreContext(DbContextOptions<StoreContext> options)
+		 : base(options)
+	{
+	}
 
-    public DbSet<rp_ef_maria.Models.Game> Game { get; set; } = default!;
+	public DbSet<rp_ef_maria.Models.Game> Game { get; set; } = default!;
+	public DbSet<rp_ef_maria.Models.Rating> Rating { get; set; } = default!;
 }
--- Models/Game.cs
+++ Models/Game.cs
@@ -1,16 +1,28 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace rp_ef_maria.Models;
 
 public class Game
 {
-    public uint GameId { get; set; }
-    public string Title { get; set; } = "";
+	public uint GameId { get; set; }
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
-    public DateTime ReleaseDate { get; set; }
-    public string Genre { get; set; } = "";
-    public decimal Price { get; set; }
+	[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Title must start with a capital letter and contain only letters, numbers, and spaces")]
+	[MaxLength(64, ErrorMessage = "Title must be 64 characters or less")]
+	public string Title { get; set; } = "";
+
+	[Display(Name = "Release Date")]
+	[DataType(DataType.Date)]
+	public DateTime ReleaseDate { get; set; }
+
+	[MaxLength(32, ErrorMessage = "Genre must be 32 characters or less")]
+	public string Genre { get; set; } = "";
+
+	[DataType(DataType.Currency)]
+	[Column(TypeName = "decimal(10,2)")]
+	public decimal Price { get; set; }
+
+	public virtual List<Rating> Ratings { get; set; } = default!;
 }
 
 
